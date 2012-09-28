53a98a0937821ae61bc894a4e101c48833680bc9
keisuke <secret.heart.ks@gmail.com>
2012-09-28 14:16:43 +0900

    modified drawperf.py

--- drawperf.py
+++ drawperf.py
@@ -38,10 +38,47 @@ def get_ioprof(fpath):
                 tmp = []
         elif val[0] == "md0":
             iops.append(float(val[3]))
-            throughput.append(float(val[5]))
+            throughput.append(float(val[5]) * 0.001)
         elif devpat.match(val[0]):
             tmp.append(float(val[11]))
     return (throughput, iops, ioutil)
+
+def ceiltop(val):
+    tmp = float(val)
+    count = 0
+    if tmp == 0.0:
+        return tmp
+    elif tmp >= 1.0:
+        while tmp >= 1.0:
+            tmp *= 0.1
+            count += 1
+        tmp *= 10
+        count -= 1
+    else:
+        while tmp < 1.0:
+            tmp *= 10
+            count -= 1
+    if tmp - int(tmp) == 0.0:
+        return int(tmp) * (10 ** count)
+    else:
+        return (int(tmp) + 1) * (10 ** count)
+
+def floortop(val):
+    tmp = float(val)
+    count = 0
+    if tmp == 0.0:
+        return tmp
+    elif val >= 1.0:
+        while tmp >= 1.0:
+            tmp *= 0.1
+            count += 1
+        tmp *= 10
+        count -= 1
+    else:
+        while tmp < 1.0:
+            tmp *= 10
+            count -= 1
+    return int(tmp) * (10 ** count)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -54,23 +91,28 @@ if __name__ == "__main__":
     fprefix = sys.argv[1].rsplit('.', 1)[0]
     gp = Gnuplot.Gnuplot()
     gp(settermcmd)
-    gp('set output "{0}"'.format("{0}thio.{1}".format(fprefix, terminaltype)))
+    fpath = "{0}thio.{1}".format(fprefix, terminaltype)
+    gp('set output "{0}"'.format(fpath))
     gp.xlabel("elapsed time(s)")
-    gp.ylabel("read throughput(B)")
+    gp.ylabel("read throughput(MB/s)")
     gp('set y2label "read iops"')
     gp('set ytics nomirror')
-    gp('set y2tics {0}'.format((max(iops) - min(iops)) / 10))
-    gp('set y2range [{0}:{1}]'.format(min(iops) * 1.01, max(iops) * 1.01))
+    maxiopsrng = ceiltop(max(iops))
+    miniopsrng = floortop(min(iops))
+    gp('set y2range [{0}:{1}]'.format(miniopsrng, maxiopsrng))
+    gp('set y2tics {0}'.format((maxiopsrng - miniopsrng) / 10))
     gdthpt = Gnuplot.Data(range(len(throughput)), throughput,
                           with_ = "lines", title = "throughput", axes = "x1y1")
     gdiops = Gnuplot.Data(range(len(iops)), iops,
                           with_ = "lines", title = "iops", axes = "x1y2")
     gp.plot(gdthpt, gdiops)
+    sys.stdout.write("output {0}\n".format(fpath))
     gp.close()
 
     gp = Gnuplot.Gnuplot()
     gp(settermcmd)
-    gp('set output "{0}"'.format("{0}util.{1}".format(fprefix, terminaltype)))
+    fpath = "{0}util.{1}".format(fprefix, terminaltype)
+    gp('set output "{0}"'.format(fpath))
     gp.xlabel("elapsed time(s)")
     gp.ylabel("util(%)")
     gdcpu = Gnuplot.Data(range(len(sysutil)), sysutil,
@@ -78,4 +120,5 @@ if __name__ == "__main__":
     gdio = Gnuplot.Data(range(len(ioutil)), ioutil,
                         with_ = "lines", title = "ioutil")
     gp.plot(gdcpu, gdio)
+    sys.stdout.write("output {0}\n".format(fpath))
     gp.close()
