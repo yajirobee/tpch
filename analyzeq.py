61652329ae6f9222b7e06cca46d22b9d6a1247f2
keisuke <secret.heart.ks@gmail.com>
2013-06-05 07:43:40 +0900

    trivial change

--- analyzeq.py
+++ analyzeq.py
@@ -64,217 +64,174 @@ def gen_allgraph(rootdir, reliddict = None, terminaltype = "png"):
                     output = "{0}iorefhist.{1}".format(outprefix, terminaltype)
                     drawioref.plot_tblrefhist(reliddict, iorefhist, output, terminaltype)
 
-def plot_workmem_exectime(dbpath, output, terminaltype = "png"):
-    gp = gpinit(terminaltype)
-    gp('set output "{0}"'.format(output))
-    gp.xlabel("working memory")
-    gp.ylabel("Time [s]")
-    gp('set grid')
-    gp('set logscale x')
-    gp('set format x "%.0s%cB"')
-    gp('set xrange [10000:*]')
-    #gp('set yrange [0:1800]')
-    if slide:
-        gp('set key right center')
-        if "eps" == terminaltype:
-            gp('set termoption font "Times-Roman,28"')
-            plotprefdict = {"with_" : "linespoints lt 1 lw 6" }
-        elif "png" == terminaltype:
-            gp('set termoption font "Times-Roman,18"')
-            plotprefdict = {"with_" : "linespoints lw 2"}
-    else:
-        plotprefdict = {"with_" : "linespoints" }
-    conn = sqlite3.connect(dbpath)
-    tbls = [r[0] for r
-            in conn.execute("select name from sqlite_master where type = 'table'")]
-    gds = []
-    query = ("select workmem, avg(exectime) from measurement "
-             "group by workmem order by workmem")
-    gds.extend(query2data(conn, query, title = "Execution time", **plotprefdict))
-    if "iotrace" in tbls:
-        query = ("select workmem, avg({y})/1000000000 from measurement, iotrace "
+class workmem_plotter(object):
+    def __init__(self, dbpath, terminaltype = "png"):
+        self.conn = sqlite3.connect(dbpath)
+        self.gp = gpinit(terminaltype)
+        self.gp.xlabel("working memory")
+        self.gp('set format x "%.0b%BB"')
+        self.gp('set logscale x')
+        self.gp('set grid')
+        if slide:
+            if "eps" == terminaltype:
+                self.gp('set termoption font "Times-Roman,28"')
+                self.plotprefdict = {"with_" : "linespoints lt 1 lw 6" }
+            elif "png" == terminaltype:
+                self.gp('set termoption font "Times-Roman,18"')
+                self.plotprefdict = {"with_" : "linespoints lw 2"}
+        else:
+            self.plotprefdict = {"with_" : "linespoints" }
+
+    def __del__(self):
+        self.gp.close()
+        self.conn.close()
+
+    def plot_workmem_exectime(self, output):
+        self.gp('set output "{0}"'.format(output))
+        self.gp.ylabel("Time [s]")
+        self.gp('set yrange [*:*]')
+        self.gp('set key right top')
+        nrow = self.conn.execute("select count(*) from iotrace").fetchone()[0]
+        gds = []
+        query = ("select workmem, avg(exectime) from measurement "
+                 "group by workmem order by workmem")
+        gds.extend(query2data(self.conn, query, title = "Execution time", **self.plotprefdict))
+        if nrow:
+            query = ("select workmem, avg({y})/1000000000 from measurement, iotrace "
+                     "where measurement.id = iotrace.id "
+                     "group by workmem order by workmem")
+            gds.extend(query2data(self.conn, query.format(y = "readio_nsec"),
+                                  title = "Read I/O cost", **self.plotprefdict))
+            gds.extend(query2data(self.conn, query.format(y = "writeio_nsec"),
+                                  title = "Write I/O cost", **self.plotprefdict))
+            gds.extend(query2data(self.conn, query.format(y = "readio_nsec + writeio_nsec"),
+                                  title = "I/O cost", **self.plotprefdict))
+            query = ("select workmem, avg(exectime - (readio_nsec + writeio_nsec) / 1000000000) "
+                     "from measurement, iotrace where measurement.id = iotrace.id "
+                     "group by workmem order by workmem")
+            gds.extend(query2data(self.conn, query, title = "CPU cost", **self.plotprefdict))
+        self.gp.plot(*gds)
+        sys.stdout.write("output {0}\n".format(output))
+
+    def plot_workmem_io(self, output):
+        nrow = self.conn.execute("select count(*) from io").fetchone()[0]
+        if not nrow:
+            return
+        self.gp('set output "{0}"'.format(output))
+        self.gp.ylabel("Total I/O size [MB]")
+        self.gp('set yrange [*:*]')
+        self.gp('set key right center')
+        query = ("select workmem, avg({y}) from measurement, io "
+                 "where measurement.id = io.id "
+                 "group by workmem order by workmem")
+        gdr = query2data(self.conn, query.format(y = "total_readmb"),
+                         title = "Read", **self.plotprefdict)[0]
+        gdw = query2data(self.conn, query.format(y = "total_writemb"),
+                         title = "Write", **self.plotprefdict)[0]
+        self.gp.plot(gdr, gdw)
+        sys.stdout.write("output {0}\n".format(output))
+
+    def plot_workmem_iocount(self, output):
+        nrow = self.conn.execute("select count(*) from iotrace").fetchone()[0]
+        if not nrow:
+            return
+        self.gp('set output "{0}"'.format(output))
+        self.gp.ylabel("I/O count")
+        self.gp('set yrange[*:*]')
+        self.gp('set key right center')
+        query = ("select workmem, avg({y}) from measurement, iotrace "
                  "where measurement.id = iotrace.id "
                  "group by workmem order by workmem")
-        gds.extend(query2data(conn, query.format(y = "readio_nsec"),
-                              title = "Read I/O cost", **plotprefdict))
-        gds.extend(query2data(conn, query.format(y = "writeio_nsec"),
-                              title = "Write I/O cost", **plotprefdict))
-        gds.extend(query2data(conn, query.format(y = "readio_nsec + writeio_nsec"),
-                              title = "I/O cost", **plotprefdict))
-        query = ("select workmem, avg(exectime - (readio_nsec + writeio_nsec) / 1000000000) "
-                 "from measurement, iotrace where measurement.id = iotrace.id "
+        gdr = query2data(self.conn, query.format(y = "readio_count"),
+                         title = "Read", **self.plotprefdict)[0]
+        gdw = query2data(self.conn, query.format(y = "writeio_count"),
+                         title = "Write", **self.plotprefdict)[0]
+        self.gp.plot(gdr, gdw)
+        sys.stdout.write("output {0}\n".format(output))
+
+    def plot_workmem_cpuutil(self, output):
+        nrow = self.conn.execute("select count(*) from cpu").fetchone()[0]
+        if not nrow:
+            return
+        self.gp('set output "{0}"'.format(output))
+        self.gp.ylabel("CPU util [%]")
+        self.gp('set yrange [0:100]')
+        self.gp('set key outside top')
+        self.gp('set style fill pattern 1 border')
+        self.conn.row_factory = sqlite3.Row
+        query = ("select workmem, avg(usr) as usr, avg(sys) as sys, avg(iowait) as iowait, "
+                 "avg(irq) as irq, avg(soft) as soft, avg(idle) as idle "
+                 "from measurement, cpu "
+                 "where measurement.id = cpu.id "
                  "group by workmem order by workmem")
-        gds.extend(query2data(conn, query, title = "CPU cost", **plotprefdict))
-    gp.plot(*gds)
-    sys.stdout.write("output {0}\n".format(output))
-    gp.close()
-
-def plot_workmem_io(dbpath, output, terminaltype = "png"):
-    conn = sqlite3.connect(dbpath)
-    tbls = [r[0] for r
-            in conn.execute("select name from sqlite_master where type = 'table'")]
-    if "io" not in tbls:
-        return
-    gp = gpinit(terminaltype)
-    gp('set output "{0}"'.format(output))
-    gp.xlabel("working memory")
-    gp.ylabel("I/O size [MB]")
-    gp('set grid')
-    gp('set logscale x')
-    gp('set format x "%.0s%cB"')
-    gp('set xrange [10000:*]')
-    #gp('set yrange [0:1800]')
-    gp('set key right center')
-    if slide:
-        if "eps" == terminaltype:
-            gp('set termoption font "Times-Roman,28"')
-            plotprefdict = {"with_" : "linespoints lt 1 lw 6" }
-        elif "png" == terminaltype:
-            gp('set termoption font "Times-Roman,18"')
-            plotprefdict = {"with_" : "linespoints lw 2"}
-    else:
-        plotprefdict = {"with_" : "linespoints" }
-    query = ("select workmem, avg({y}) from measurement, io "
-             "where measurement.id = io.id "
-             "group by workmem order by workmem")
-    gdr = query2data(conn, query.format(y = "total_readmb"),
-                     title = "Read", **plotprefdict)[0]
-    gdw = query2data(conn, query.format(y = "total_writemb"),
-                     title = "Write", **plotprefdict)[0]
-    gp.plot(gdr, gdw)
-    sys.stdout.write("output {0}\n".format(output))
-    gp.close()
-
-def plot_workmem_iocount(dbpath, output, terminaltype):
-    conn = sqlite3.connect(dbpath)
-    tbls = [r[0] for r
-            in conn.execute("select name from sqlite_master where type = 'table'")]
-    if "iotrace" not in tbls:
-        return
-    gp = gpinit(terminaltype)
-    gp('set output "{0}"'.format(output))
-    gp.xlabel("working memory")
-    gp.ylabel("I/O count")
-    gp('set grid')
-    gp('set logscale x')
-    gp('set format x "%.0s%cB"')
-    gp('set xrange [10000:*]')
-    #gp('set yrange [0:1800]')
-    gp('set key right center')
-    if slide:
-        if "eps" == terminaltype:
-            gp('set termoption font "Times-Roman,28"')
-            plotprefdict = {"with_" : "linespoints lt 1 lw 6" }
-        elif "png" == terminaltype:
-            gp('set termoption font "Times-Roman,20"')
-            plotprefdict = {"with_" : "linespoints lw 2"}
-    else:
-        plotprefdict = {"with_" : "linespoints" }
-    query = ("select workmem, avg({y}) from measurement, iotrace "
-             "where measurement.id = iotrace.id "
-             "group by workmem order by workmem")
-    gdr = query2data(dbpath, query.format(y = "readio_count"),
-                     title = "Read", **plotprefdict)[0]
-    gdw = query2data(dbpath, query.format(y = "writeio_count"),
-                     title = "Write", **plotprefdict)[0]
-    gp.plot(gdr, gdw)
-    sys.stdout.write("output {0}\n".format(output))
-    gp.close()
-
-def plot_workmem_cpuutil(dbpath, output, terminaltype):
-    conn = sqlite3.connect(dbpath)
-    tbls = [r[0] for r
-            in conn.execute("select name from sqlite_master where type = 'table'")]
-    if "cpu" not in tbls:
-        return
-    gp = gpinit(terminaltype)
-    gp('set output "{0}"'.format(output))
-    gp.xlabel("working memory")
-    gp.ylabel("CPU util [%]")
-    gp('set grid')
-    gp('set logscale x')
-    gp('set format x "%.0s%cB"')
-    gp('set xrange [10000:*]')
-    gp('set yrange [0:100]')
-    gp('set key outside')
-    gp('set style fill pattern 1 border')
-    conn = sqlite3.connect(dbpath)
-    conn.row_factory = sqlite3.Row
-    query = ("select workmem, avg(usr) as usr, avg(sys) as sys, avg(iowait) as iowait, "
-             "avg(irq) as irq, avg(soft) as soft, avg(idle) as idle "
-             "from measurement, cpu "
-             "where measurement.id = cpu.id "
-             "group by workmem order by workmem")
-    cur = conn.cursor()
-    cur.execute(query)
-    r = cur.fetchone()
-    keys = r.keys()
-    xlist = []
-    piledatas = [[] for i in range(len(keys[1:]))]
-    xlist.append(r[0])
-    piledatas[0].append(r[1])
-    for i in range(2, len(keys)):
-        piledatas[i - 1].append(piledatas[i - 2][-1] + r[i])
-    for r in cur:
+        cur = self.conn.cursor()
+        cur.execute(query)
+        r = cur.fetchone()
+        keys = r.keys()
+        xlist = []
+        piledatas = [[] for i in range(len(keys[1:]))]
         xlist.append(r[0])
         piledatas[0].append(r[1])
         for i in range(2, len(keys)):
             piledatas[i - 1].append(piledatas[i - 2][-1] + r[i])
-    gds = []
-    for k, dat in zip(keys[:0:-1], piledatas[::-1]):
-        gds.append(Gnuplot.Data(xlist, dat, [v / 4 for v in xlist], title = k,
-                               with_ = 'boxes fs solid border lc rgb "black"'))
-    gp.plot(*gds)
-    sys.stdout.write("output {0}\n".format(output))
-    gp.close()
+        for r in cur:
+            xlist.append(r[0])
+            piledatas[0].append(r[1])
+            for i in range(2, len(keys)):
+                piledatas[i - 1].append(piledatas[i - 2][-1] + r[i])
+        gds = []
+        for k, dat in zip(keys[:0:-1], piledatas[::-1]):
+            gds.append(Gnuplot.Data(xlist, dat, [v / 4 for v in xlist],
+                                    title = k,
+                                    with_ = 'boxes fs solid border lc rgb "black"'))
+        self.gp.plot(*gds)
+        sys.stdout.write("output {0}\n".format(output))
+        self.conn.row_factory = None
 
-def plot_workmem_cputime(dbpath, output, terminaltype):
-    gp = gpinit(terminaltype)
-    gp('set output "{0}"'.format(output))
-    gp.xlabel("working memory")
-    gp.ylabel("Time [s]")
-    gp('set grid')
-    gp('set logscale x')
-    gp('set format x "%.0s%cB"')
-    gp('set xrange [10000:*]')
-    gp('set yrange [0:*]')
-    gp('set key outside')
-    gp('set style fill pattern 1 border')
-    conn = sqlite3.connect(dbpath)
-    conn.row_factory = sqlite3.Row
-    query = ("select workmem, avg(exectime) as exectime, "
-             "avg(usr) / 100 as usr, avg(sys) / 100 as sys, "
-             "avg(iowait) / 100 as iowait, avg(irq) / 100 as irq, "
-             "avg(soft) / 100 as soft, avg(idle) / 100 as idle "
-             "from measurement, cpu "
-             "where measurement.id = cpu.id "
-             "group by workmem order by workmem")
-    cur = conn.cursor()
-    cur.execute(query)
-    r = cur.fetchone()
-    keys = r.keys()
-    xlist = []
-    piledatas = [[] for i in range(len(keys[2:]))]
-    xlist.append(r[0])
-    piledatas[0].append(r[2])
-    for i in range(3, len(keys)):
-        piledatas[i - 2].append(piledatas[i - 3][-1] + r[i])
-    for i in range(len(keys[2:])):
-        piledatas[i][-1] *= r[1]
-    for r in cur:
+    def plot_workmem_cputime(self, output):
+        nrow = self.conn.execute("select count(*) from cpu").fetchone()[0]
+        if not nrow:
+            return
+        self.gp('set output "{0}"'.format(output))
+        self.gp.ylabel("Time [s]")
+        self.gp('set yrange [0:*]')
+        self.gp('set key outside top')
+        self.gp('set style fill pattern 1 border')
+        self.conn.row_factory = sqlite3.Row
+        query = ("select workmem, avg(exectime) as exectime, "
+                 "avg(usr) / 100 as usr, avg(sys) / 100 as sys, "
+                 "avg(iowait) / 100 as iowait, avg(irq) / 100 as irq, "
+                 "avg(soft) / 100 as soft, avg(idle) / 100 as idle "
+                 "from measurement, cpu "
+                 "where measurement.id = cpu.id "
+                 "group by workmem order by workmem")
+        cur = self.conn.cursor()
+        cur.execute(query)
+        r = cur.fetchone()
+        keys = r.keys()
+        xlist = []
+        piledatas = [[] for i in range(len(keys[2:]))]
         xlist.append(r[0])
         piledatas[0].append(r[2])
         for i in range(3, len(keys)):
             piledatas[i - 2].append(piledatas[i - 3][-1] + r[i])
         for i in range(len(keys[2:])):
             piledatas[i][-1] *= r[1]
-    gds = []
-    for k, dat in zip(keys[:0:-1], piledatas[::-1]):
-        gds.append(Gnuplot.Data(xlist, dat, [v / 4 for v in xlist], title = k,
-                               with_ = 'boxes fs solid border lc rgb "black"'))
-    gp.plot(*gds)
-    sys.stdout.write("output {0}\n".format(output))
-    gp.close()
+        for r in cur:
+            xlist.append(r[0])
+            piledatas[0].append(r[2])
+            for i in range(3, len(keys)):
+                piledatas[i - 2].append(piledatas[i - 3][-1] + r[i])
+            for i in range(len(keys[2:])):
+                piledatas[i][-1] *= r[1]
+        gds = []
+        for k, dat in zip(keys[:0:-1], piledatas[::-1]):
+            gds.append(Gnuplot.Data(xlist, dat, [v / 4 for v in xlist],
+                                    title = k,
+                                    with_ = 'boxes fs solid border lc rgb "black"'))
+        self.gp.plot(*gds)
+        sys.stdout.write("output {0}\n".format(output))
+        self.conn.row_factory = None
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -300,13 +257,14 @@ if __name__ == "__main__":
 
     reliddict = get_reliddict(relidfile) if relidfile else None
     #gen_allgraph(rootdir, reliddict, terminaltype)
+    wp = workmem_plotter(rootdir + "/spec.db", terminaltype)
     output = "{0}/exectime.{1}".format(rootdir, terminaltype)
-    plot_workmem_exectime(rootdir + "/spec.db", output, terminaltype)
+    wp.plot_workmem_exectime(output)
     output = "{0}/io.{1}".format(rootdir, terminaltype)
-    plot_workmem_io(rootdir + "/spec.db", output, terminaltype)
+    wp.plot_workmem_io(output)
     output = "{0}/iocount.{1}".format(rootdir, terminaltype)
-    plot_workmem_iocount(rootdir + "/spec.db", output, terminaltype)
+    wp.plot_workmem_iocount(output)
     output = "{0}/cpuutil.{1}".format(rootdir, terminaltype)
-    plot_workmem_cpuutil(rootdir + "/spec.db", output, terminaltype)
+    wp.plot_workmem_cpuutil(output)
     output = "{0}/cputime.{1}".format(rootdir, terminaltype)
-    plot_workmem_cputime(rootdir + "/spec.db", output, terminaltype)
+    wp.plot_workmem_cputime(output)
