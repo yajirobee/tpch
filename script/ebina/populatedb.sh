256cbf95cd71bf89b61ed0936eef942fe12f8695
keisuke <keisuke@tkl.iis.u-tokyo.ac.jp>
2013-12-09 14:51:41 +0900

    trivial change

--- script/ebina/populatedb.sh
+++ script/ebina/populatedb.sh
@@ -3,9 +3,9 @@
 pgbindir=/data/local/keisuke/local/bin
 dbname=${1}
 
-${pgbindir}/createdb --username=keisuke --tablespace=iod8raid0 ${dbname}
-${pgbindir}/psql --username=keisuke --dbname=${dbname} --file=createtable.sql
+#${pgbindir}/createdb --username=keisuke --tablespace=iod8raid0 ${dbname}
+${pgbindir}/psql --username=keisuke --dbname=${dbname} --file=../createtable.sql
 ${pgbindir}/psql --username=keisuke --dbname=${dbname} --file=loaddata.sql
-${pgbindir}/psql --username=keisuke --dbname=${dbname} --file=createindex.sql
+${pgbindir}/psql --username=keisuke --dbname=${dbname} --file=../createindex.sql
 
 exit 0
